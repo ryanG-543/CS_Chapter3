8c6251e8d9b9a2b6e91f45740cd60c280926a45a
ryanG-543 <ryangiang29@gmail.com>
2017-02-08 14:15:44 -0500

    Completed CheckLowRate

--- CS_Chapter3/CheckLowRate/Program.cs
+++ CS_Chapter3/CheckLowRate/Program.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace CheckLowRate
 {
-    class Program
+    class IfElseDecision
     {
         static void Main(string[] args)
         {
+            const double HIGH = 5.65;
+            string payString;
+            int pay;
+            Console.WriteLine("What is the hourly pay?");
+            payString = Console.ReadLine();
+            pay = Convert.ToInt32(payString);
+            if (pay > HIGH)
+                Console.WriteLine("Excellent");
+            else
+                Console.WriteLine("ERROR. PAY MUST BE GREATER THAN ${0}", HIGH);
         }
     }
 }
