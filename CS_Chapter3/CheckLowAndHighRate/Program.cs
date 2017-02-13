5bdf9e104b2673aad1f479b933bf16a23b63779d
ryanG-543 <ryangiang29@gmail.com>
2017-02-13 14:15:37 -0500

    Completed CheckLowAndHighRate

--- CS_Chapter3/CheckLowAndHighRate/Program.cs
+++ CS_Chapter3/CheckLowAndHighRate/Program.cs
@@ -9,6 +9,20 @@ namespace CheckLowAndHighRate
     {
         static void Main(string[] args)
         {
+            const double LOW = 5.65;
+            const double HIGH = 49.99;
+            string payString;
+            int pay;
+            Console.WriteLine("What is the hourly pay?");
+            payString = Console.ReadLine();
+            pay = Convert.ToInt32(payString);
+            if (pay < LOW)
+                Console.WriteLine("ERROR. NOT ENOUGH MONEY");
+            else
+                if (pay > HIGH)
+                Console.WriteLine("ERROR. TOO MUCH MONEY");
+            else
+                Console.WriteLine("EXCELLENT");
         }
     }
 }
