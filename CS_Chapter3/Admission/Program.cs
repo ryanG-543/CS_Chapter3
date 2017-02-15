832cd25d5511323bd7beb8f1a71523966105f998
ryanG-543 <ryangiang29@gmail.com>
2017-02-15 14:15:40 -0500

    Completed Furniture and Admission

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,7 +9,29 @@ namespace Admission
     {
         static void Main(string[] args)
         {
-            
+            double mingpa = 3.0;
+            double lowScore = 60;
+            double highScore = 80;
+            string gpaString;
+            string scoreString;
+            double gpa;
+            double score;
+
+            //USER PROMPT -- GPA
+            Console.WriteLine("What is your GPA?");
+            gpaString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaString);
+
+            //USER PROMPT -- TEST SCORE
+            Console.WriteLine("What was your test score?");
+            scoreString = Console.ReadLine();
+            score = Convert.ToDouble(scoreString);
+
+            //RESULTS
+            if (gpa >= mingpa && score >= lowScore || gpa < mingpa && score >= highScore)
+                Console.WriteLine("Accepted");
+            else 
+                Console.WriteLine("Denied");
         }
     }
 }
--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,33 +9,33 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
-            char p = 100;
-            char o = 225;
-            char m = 310;
+            int p = 100;
+            int o = 225;
+            int m = 310;
             char wood;
             string woodString;
 
             // PROPMPT USER FOR WOOD TYPE
             Console.WriteLine("What kind of wood do you want?");
-            woodString = Console.ReadLine();
+            woodString = Console.ReadLine().ToLower();
             wood = Convert.ToChar(woodString);
 
-            // PINE
-            if (wood == p)
-                Console.WriteLine("The cost of a pine table is ${0}", p);
-
-            // OAK
-            if (wood == o)
-                Console.WriteLine("The cost of an oak table is ${0}", o);
-
-            // MAHOGANY
-            if (wood == m)
-                Console.WriteLine("The cost of a mahogany table is ${0}", m);
-
-            // NONE
-            if (wood != m || wood != o || wood != p)
-                Console.WriteLine("Your cost is $0");
-
+            //SWITCH STATEMENT
+            switch(wood)
+            { 
+                case 'p':
+                    Console.WriteLine("The cost of a pine table is ${0}", p);
+                        break;
+                case 'o':
+                    Console.WriteLine("The cost of an oak table is ${0}", o);
+                        break;
+                case 'm':
+                    Console.WriteLine("The cost of a mahogany table is ${0}", m);
+                        break;
+                default:
+                    Console.WriteLine("Your cost is $0");
+                        break;
+            }
         }
     }
 }
