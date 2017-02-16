63a3026eaa9d23e00a0647f17f95d30350c33cb2
ryanG-543 <ryangiang29@gmail.com>
2017-02-16 14:20:36 -0500

    Worked on Payroll

--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,6 +9,30 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+            double hourPay;
+            double hourWork;
+            double grossPay; // = hourPay * hourWork;
+            double withTax;
+            double netPay;
+
+            // USER PROMPT
+            Console.Write("Enter your hourly pay: ");
+            hourPay = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the hours you worked");
+            hourWork = Convert.ToDouble(Console.ReadLine());
+
+            grossPay = hourPay * hourWork;
+
+            // WITHHOLDING TAX
+            if (grossPay <= 300)
+                withTax = grossPay * 0.10;
+            else
+                withTax = grossPay * 0.12;
+
+            netPay = grossPay - withTax;
+
+            Console.WriteLine
         }
     }
 }
